e0aa05ae42b8bc0026e4fd527be7e88fba7a45dd
Isak Ernstig <ernstig1@gmail.com>
2018-11-27 10:33:06 +0100

    Created a Main class

---
+++ src/Main.py
@@ -0,0 +1,11 @@
+
+# suggested naming conventions:
+# define classes beginning with uppercase letters and use CamelCase:
+# example: RatClass
+# define private class variables starting with an underscore:
+# example: _private_variable
+
+
+
+if __name__ == '__main__':
+    print("hello complex simulations!")
