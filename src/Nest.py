1070330a68de0966aed0d28be06edc747d47bd0f
RomStriker <RomStriker@users.noreply.github.com>
2018-11-30 15:14:24 +0100

    Update Nest.py
    Commenting stuff not needed for basic model.

--- src/Nest.py
+++ src/Nest.py
@@ -1,18 +1,30 @@
+import numpy as np
+from AgentSuper import AgentSuper
 
-class Nest:
-    def __init__(self, x, y, id, ht):
+
+'''Nest Class'''
+
+
+class Nest(AgentSuper):
+    def __init__(self, grid_size, x, y, life_time):
+        AgentSuper.__init__(self, grid_size, x, y, life_time)
         self.x = x
         self.y = y
-        self.parent_id = id
+        '''
+        parent, ht, 
+        self.parent_id = parent
         self.hatch_time = ht
-        self.count_time = 0
+        self.incubation_time = 0
 
     def move(self):
-        self.count_time += 1
+        self.incubation_time += 1
+        
+    def check_hatch(self)
+        return self.incubation_time>hatch_time
 
     def hatch(self):
         lattice.spawn(self)
 
     def die(self):
         lattice.nest_list(self, remove)
-        del self
+        del self'''
