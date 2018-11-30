b02c4fdf03cf1bc4667e3a1aa66e2fe761dc5807
parvid <parvid@student.chalmers.se>
2018-11-30 16:31:53 +0100

    Update Bird.py

--- src/Bird.py
+++ src/Bird.py
@@ -1,13 +1,2 @@
 
-from SuperAgent import SuperAgent
 
-class Bird(SuperAgent):
-     def __init__(self, x, y):
-         super().__init__(self, x, y)
-         self.x_nest
-         self.y_nest
-         self.is_in_nest
-         self.nest
-         self.has_nest = False
-         self.x = x
-         self.y = y
