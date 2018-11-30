6b52d1a6b70771792e6c892a4276c33cfb77a64c
RomStriker <RomStriker@users.noreply.github.com>
2018-11-30 14:33:23 +0100

    Update Nest.py

--- src/Nest.py
+++ src/Nest.py
@@ -1,11 +1,11 @@
 
 class Nest:
-    def __init__(self, x, y, id1, id2, ht, ct):
+    def __init__(self, x, y, id1, id2, ht):
         self.x = x
         self.y = y
         self.parent_id = id
         self.hatch_time = ht
-        self.count_time = ct
+        self.count_time = 0
 
     def move(self):
         self.count_time += 1
@@ -15,4 +15,4 @@ class Nest:
 
     def die(self):
         del self
-        lattice.nest_list(self,remove)
+        lattice.nest_list(self, remove)
