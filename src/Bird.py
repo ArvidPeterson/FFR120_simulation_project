62ec1ace4a540d7249bf2bf6091f401b59285838
katarint <35830424+katarint@users.noreply.github.com>
2018-12-02 20:01:51 +0100

    updated Bird
    updated place_nest method
    added  move

--- src/Bird.py
+++ src/Bird.py
@@ -1,66 +1,57 @@
 from AgentSuper import AgentSuper
 from Rat import Rat
+from Nest import Nest
 import numpy as np
 
 
 class Bird(AgentSuper):
-
-
-    def __init__(self, x_nest = None, y_nest = None):
-         super().__init__(self)
+    def __init__(self, grid_size, x, y, life_time = 1):
+         AgentSuper.__init__(self, grid_size, x, y, life_time)
          #all_positions =
          self.is_in_nest = True
          self.has_nest = False
 
-
-    def place_nest(self,nest_list,all_positions):
-         available_positions = np.delete(all_positions,nest_list)
-         pick_position = np.random.randit(0,len(available_positions)-1)
-         nest_x = available_positions(pick_position, 1)
-         nest_y = available_positions(pick_position, 2)
-
-
-         return nest_x,nest_y
-
     def check_is_in_nest(self):
         return self.is_in_nest
 
-    def __init__(self, x, y, grid_size):
-        super().__init__(self, x, y)
-        self.x_nest
-        self.y_nest
-        self.is_in_nest
-        self.nest
-        self.has_nest = False
-        self.x = x
-        self.y = y
-        self.grid_size = grid_size
-
-    def place_nest(self,nest_list,all_positions):
-         available_positions = np.delete(all_positions,nest_list)
-         pick_position = np.random.randit(0,len(available_positions)-1)
-         nest_x = available_positions(pick_position, 1)
-         nest_y = available_positions(pick_position, 2)
-
-         return nest_x, nest_y
-
-    def check_is_in_nest(self):
-         return self.is_in_nest
+    def move(self):
+        r = np.random.randint(0, 3)    # the bird spents 1/3 of the time away from nest in this model
+        if r == 0:
+            self.is_in_nest = False
+        else:
+            self.is_in_nest = True
 
     def coordinate_to_index(self, coord):
         x, y = coord
         return x + y * self.grid_size
 
     def index_to_coordinate(self, index):
-        pass
+        x = index % self.grid_size
+        y = int(np.floor(index/self.grid_size))
+        return (x, y)
+
 
     def place_nest(self, nest_list):
-        all_indices = np.linspace(0,self.grid_size, self.grid_size - 1, dtype=int )
+        # Creates a temporary grid used to determine where nests can be created.
+        all_indices = range(self.grid_size**2)
         nest_indices = []
         for nest in nest_list:
-            nest_index = self.coordinate_to_index((nest.x, nest.y))
-            nest_indices.append(nest_index)
-        allowed_indices = np.delete(all_indices, nest_indices)
+            # The coordinates of the current nest is turned into an index. This index is added to the list of indeces where nests exist.
+            nest_indices.append(self.coordinate_to_index((nest.x, nest.y)))
+        available_indices = np.delete(all_indices, nest_indices)
+        random_index = np.random.randint(0, available_indices.__len__())
+        (x_new_nest, y_new_nest) = self.index_to_coordinate(available_indices[random_index])
+
+        # ==========================================================
+        some_life_time = 1 # THIS SHOULD BE SET TO THE PROPER VALUE
+        # ==========================================================
+
+        # A new nest object is created
+        new_nest = Nest(self.grid_size, x_new_nest, y_new_nest, some_life_time)
+        return new_nest
+
+
+
 
 
 
--- src/test.py
+++ src/test.py
@@ -1,3 +1,30 @@
 
+from Bird import Bird
+from Nest import Nest
+
+gridSize = 3
+
+bird = Bird(gridSize, 4, 2)
+
+
+nest1 = Nest(gridSize, 0, 2)
+nest2 = Nest(gridSize, 1, 2)
+nest3 = Nest(gridSize, 1, 0)
+nest_list = []
+nest_list.append(nest1)
+nest_list.append(nest2)
+nest_list.append(nest3)
+
+new_nest = bird.place_nest(nest_list)
+
+for nest in nest_list:
+    print(nest.x)
+    print(nest.y)
+    print(' ')
+
+
+print(new_nest.x)
+print(new_nest.y)
+print('complete')
 
 
