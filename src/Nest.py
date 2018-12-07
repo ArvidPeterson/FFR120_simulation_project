aadcab4dbe1bb837764034efc310ec258ac9c3f9
ArvidPeterson <parvid@student.chalmers.se>
2018-12-07 13:48:10 +0100

    fix in nest

--- src/Nest.py
+++ src/Nest.py
@@ -7,4 +7,9 @@ from AgentSuper import AgentSuper
 class Nest(AgentSuper):
     def __init__(self, grid_size, x, y, life_time):
         AgentSuper.__init__(self, grid_size, x, y, life_time)
+        self.hatch_time = self.life_time
+        self.counter = 0
 
+
+    def move(self):
+        self.counter += 1
