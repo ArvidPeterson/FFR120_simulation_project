6888b815ed6464afb6713aca2f2ddf40d94ba6b8
Isak Ernstig <ernstig1@gmail.com>
2018-12-17 13:34:35 +0100

    now the parameter sweeps are working

--- src/ParameterSweep.py
+++ src/ParameterSweep.py
@@ -35,13 +35,26 @@ class ParamSweep:
                                        self.nutritional_value, plot_environment=False,
                                        plot_populations=False)
                 self.sim.start()
-                data = self.sim.join()
-                self.save_plot('test_plot' + str(np.random.randint(100)))
-                plt.close(self.sim.fig)
+                bird_pop, rat_pop, nest_pop, time = self.sim.join()
+                fig, ax = plt.subplots()
+                plt.plot(time, bird_pop, color='blue', label='Bird population')
+                plt.plot(time, nest_pop, color='green', label='Nest population')
+                plt.plot(time, rat_pop, color='red', label='Rat population')
+                plt.legend()
+                ax.set_xlabel('Time steps')
+                ax.set_ylabel('Populations')
+                ax.set_title('initial bird population: {}, initial rat population: {}\n'
+                             'nest placement delay: {}, nest nutritional value: {}'.format(
+                    nbirds, nrats, self.nest_placement_delay, self.nutritional_value
+                ))
+                fname = 'nbirds{}nrats{}'.format(nbirds, nrats)
+                self.save_plot(fig, fname)
 
-    def save_plot(self, name):
+                #plt.close(fig)
+
+    def save_plot(self, fig, name):
         dir = 'save_data/' + str(name) + '.png'
-        self.sim.fig.savefig(dir)
+        fig.savefig(dir)
 
 
 if __name__ == '__main__':
@@ -50,9 +63,9 @@ if __name__ == '__main__':
     nutritional_value = 10
     rat_initial_energy = 100
     nest_placement_delay = 100
-    n_sim_steps = int(1e3)
-    n_rats_range = [5, 10]
-    n_birds_range = [200, 500]
+    n_sim_steps = int(1e2)
+    n_rats_range = [5]
+    n_birds_range = [20]
     sweep = ParamSweep(lattice_size, n_sim_steps, n_birds_range,
                        n_rats_range, hatch_time, rat_initial_energy,
                        nutritional_value, nest_placement_delay)
